c1291e23383c6d75c7fea2f51b4b26db90d0e37c
KauaFrenedozo10 <kfrenedoso@gmail.com>
2026-08-18 19:32:56 -0300

    UseState

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,9 +1,24 @@
-import Botao from "./components/addCount"
+import { useState } from "react";
+import Botao from "./components/addCount";
 
 function App() {
+  const [total, setTotal] = useState(0);
+
+  function Somatotal() {
+    setTotal(total + 1);
+  }
+
   return (
-    <div style={{ backgroundColor: "blue"}}>
-        <Botao/>
+    <div style={{ padding: "20px" }}>
+      <Botao titulo="Risco na superficie" onRegistrar={Somatotal} />
+      <Botao titulo="Dimensão fora do padrão" onRegistrar={Somatotal} />
+      <Botao titulo="Peça trincada" onRegistrar={Somatotal} />
+
+      <p>
+        {total === 0
+          ? "Nenhum defeito registrado até o momento."
+          : `Total: ${total}`}
+      </p>
     </div>
   );
 }
--- usestate/src/components/addCount.jsx
+++ usestate/src/components/addCount.jsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
 
-function Botao() {
-    const [count, setCount] = useState(0)
+function Botao({titulo, onRegistrar}) {
+    const [count, setCount] = useState(0);
 
     function ClickNoBotao() {
         setCount(count + 1);
+        onRegistrar();
     }
+   
 
     return (
-        <button onClick={ClickNoBotao}>
-            {count} Cliques foi registrado
-        </button>
+        <div style={{
+            width: "80px",
+            padding: "10px",
+            border: "1px solid #ddd",
+            borderRadius: "4px",
+            marginBottom: "5px"
+        }}>
+            <span style={{ fontSize: "14px", fontWeight: "500"}}>
+                {titulo} - <strong>{count}</strong>
+            </span>
+            
+            <button 
+            
+                onClick={ClickNoBotao}
+                
+                style={{
+                    backgroundColor: "#087ff5",
+                    color: "white",
+                    border: "none",
+                    padding: "6px 12px",
+                    borderRadius: "3px",
+                    cursor: "pointer"
+                }}
+            >
+                Registrar
+            </button>
+        </div>
     );
 }
+
 export default Botao;
