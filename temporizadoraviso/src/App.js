75fd958f552e3b147f96b582c13a97edf8fe93e5
KAUÃ HENRIQUE FRENEDOZO <kaua.frenedozo@senaisp.edu.br>
2026-09-08 20:30:24 -0300

    Temporizador

---
+++ temporizadoraviso/src/App.js
@@ -0,0 +1,25 @@
+
+import { useEffect, useState } from "react";
+
+function AvisoTemporizado() {
+  const [visivel, setVisivel] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisivel(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      {visivel && <p>Bem-vindo!</p>}
+    </div>
+  );
+}
+
+export default AvisoTemporizado;
+
